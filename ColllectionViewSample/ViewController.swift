1f6683aae8f7261ffb5b1a9ab9e0afa3f89b9dae
SHABEERALI K <shabeeralik@SHABEERALIs-MacBook-Pro.local>
2018-12-18 22:21:01 +0530

    First commit

---
+++ ColllectionViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColllectionViewSample
+//
+//  Created by SHABEERALI K on 18/12/18.
+//  Copyright © 2018 SHABEERALI K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
